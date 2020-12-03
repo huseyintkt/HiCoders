1461f37071b6eb1e2dd5ccc0e900dcbcd038bdcb
= <huseyintkt41@gmail.com>
2020-12-03 19:29:49 +0100

    Cinema_Room_Manager stage-1

---
+++ Cinema_Room_Manager/stage-1.js
@@ -0,0 +1,47 @@
+/**
+ *  Description
+    There are many enjoyable activities on this funny little planet Earth, and still, the happiest, simplest, and most fulfilling one is probably going to the movies. Going with friends, going with loved ones, experiencing a whole new adventure from the safety of a cinema seat, surrounded by like-minded fellow travelers.
+
+    As a beginner developer, you can contribute to creating this wonderful cinema experience. Your good friends asked you to help them create an application for a cinema theatre where people can get tickets, reserve seats, and enjoy their movie night.
+
+    Objectives
+    There is not a lot of space in our new cinema theatre, so you need to take into account the restrictions on the number of seats. Your friends said that the room would fit 7 rows of 8 seats. Your task is to help them visualize the seating arrangement by printing the scheme to the console.
+
+    Your output should be like in the example below and should contain 9 lines.
+
+    Example
+
+    Cinema:
+    1 2 3 4 5 6 7 8
+    1 S S S S S S S S
+    2 S S S S S S S S
+    3 S S S S S S S S
+    4 S S S S S S S S
+    5 S S S S S S S S
+    6 S S S S S S S S
+    7 S S S S S S S S
+ */
+
+
+
+console.log("Cinema : ");
+
+let firstLine = "   ";
+
+for(let k=1; k<9; k++){
+    firstLine += k + " ";
+}
+
+console.log(firstLine);
+
+let line = " ";
+
+for(let i=1; i<8; i++){
+    line = line + i;
+    for(let j=1; j<9; j++){
+        line = line + " S";
+    }
+
+    console.log(line);
+    line = " ";
+}
